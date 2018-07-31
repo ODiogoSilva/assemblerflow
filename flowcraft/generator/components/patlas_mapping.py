9507ac09cc7d1cdee2440a93e14777dc21c9bbf5
tiagofilipe12 <tiagofilipe12@gmail.com>
2018-07-31 23:41:44 +0100

    changed docker image tag version

--- flowcraft/generator/components/patlas_mapping.py
+++ flowcraft/generator/components/patlas_mapping.py
@@ -47,14 +47,14 @@ class PatlasMapping(Process):
         self.directives = {
             "mappingBowtie": {
                 "container": "flowcraft/mapping-patlas",
-                "version": "1.5.2-1",
+                "version": "1.5.2-2",
                 "cpus": 1,
                 "memory": "{ 4.GB * task.attempt }",
                 "scratch": "true"
             },
             "jsonDumpingMapping": {
                 "container": "flowcraft/mapping-patlas",
-                "version": "1.5.2-1",
+                "version": "1.5.2-2",
                 "cpus": 1,
                 "memory": "'4GB'"
             }
