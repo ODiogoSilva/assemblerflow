7e61d751883b666fe8ec7992eb0dbe15cb921047
cimendes <iines.mendes@gmail.com>
2018-12-16 17:48:11 +0000

    update tests

--- flowcraft/tests/test_broadcast.py
+++ flowcraft/tests/test_broadcast.py
@@ -2,17 +2,17 @@ import pytest
 import os
 
 import flowcraft.generator.utils as utils
-from flowcraft.generator.error_handling import InspectionError
+from flowcraft.generator.error_handling import LogError
 
 
 def test_empty_log():
-    with pytest.raises(InspectionError):
+    with pytest.raises(LogError):
         utils.get_nextflow_filepath(
             os.path.join(os.getcwd(), "flowcraft/tests/broadcast_tests/empty_log.txt"))
 
 
 def test_no_path_in_log():
-    with pytest.raises(InspectionError):
+    with pytest.raises(LogError):
         utils.get_nextflow_filepath(
             os.path.join(os.getcwd(), "flowcraft/tests/broadcast_tests/log_without_command.txt"))
 
